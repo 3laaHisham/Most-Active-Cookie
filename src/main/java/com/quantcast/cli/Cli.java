d47dfcbf1c24f91656fdfb95d39d48b09b48d0e9
3laaHisham <alaa.ismail@student.guc.edu.eg>
2025-05-25 13:06:12 +0200

    refactor: enforce required options for file and date in command line parser

--- src/main/java/com/quantcast/cli/Cli.java
+++ src/main/java/com/quantcast/cli/Cli.java
@@ -7,18 +7,14 @@ import org.apache.commons.cli.*;
 public class Cli {
 
     private final Options options;
-    private final HelpFormatter help;
+
     public Cli() {
         this.options = defineOptions();
-        this.help = new HelpFormatter();
     }
 
     public Config parse(String[] args) throws ParseException, IllegalArgumentException {
         CommandLineParser parser = new DefaultParser();
         CommandLine cmd = parser.parse(options, args);
-
-        if (!cmd.hasOption("f") || !cmd.hasOption("d"))
-            throw new ParseException("Missing required options " + options.toString());
 
         String filePath = cmd.getOptionValue("f");
         DateRange dateRange = DateRangeParser.parse(cmd.getOptionValue("d"));
@@ -28,17 +24,11 @@ public class Cli {
 
     }
 
-
-    private void printAndExit(int exitCode) {
-        help.printHelp("most-active-cookie", options);
-        System.exit(exitCode);
-    }
-
     private Options defineOptions() {
         Options opts = new Options();
 
-        opts.addOption("f", "file", true, "Path to cookie log CSV (required)");
-        opts.addOption("d", "date", true, "Single or range date, e.g. 2025-05-16 or 2025-05-10:2025-05-12");
+        opts.addRequiredOption("f", "file", true, "Path to cookie log CSV (required)");
+        opts.addRequiredOption("d", "date", true, "Single or range date, e.g. 2025-05-16 or 2025-05-10:2025-05-12");
         opts.addOption("t", "top", true, "Top N ranks (default 1)");
         opts.addOption("h", "help", false, "Show help");
 
